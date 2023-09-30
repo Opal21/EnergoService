b647037c8f11415ca625a908f7300e2fbd237419
Marcin Opalinski <marcin.opalinski@grupa-abs.pl>
2023-09-30 10:35:12 +0200

    Implement all new repositories

--- app/src/main/java/pl/energosystem/energoservice/data/AppContainer.kt
+++ app/src/main/java/pl/energosystem/energoservice/data/AppContainer.kt
@@ -1,12 +1,23 @@
 package pl.energosystem.energoservice.data
 
 import android.content.Context
+import pl.energosystem.energoservice.data.protocol.OfflineProtocolsRepository
+import pl.energosystem.energoservice.data.protocol.ProtocolsDatabase
+import pl.energosystem.energoservice.data.protocol.ProtocolsRepository
+import pl.energosystem.energoservice.data.task.OfflineTasksRepository
+import pl.energosystem.energoservice.data.task.TasksDatabase
+import pl.energosystem.energoservice.data.task.TasksRepository
+import pl.energosystem.energoservice.data.user.OfflineUsersRepository
+import pl.energosystem.energoservice.data.user.UsersDatabase
+import pl.energosystem.energoservice.data.user.UsersRepository
 
 /**
  * App container for Dependency injection.
  */
 interface AppContainer {
     val protocolsRepository: ProtocolsRepository
+    val tasksRepository: TasksRepository
+    val usersRepository: UsersRepository
 }
 
 /**
@@ -19,5 +30,19 @@ class AppDataContainer(private val context: Context) : AppContainer {
      */
     override val protocolsRepository: ProtocolsRepository by lazy {
         OfflineProtocolsRepository(ProtocolsDatabase.getDatabase(context).protocolDao())
+    }
+
+    /**
+     * Implementation for [TasksRepository]
+     */
+    override val tasksRepository: TasksRepository by lazy {
+        OfflineTasksRepository(TasksDatabase.getDatabase(context).taskDao())
+    }
+
+    /**
+     * Implementation for [UsersRepository]
+     */
+    override val usersRepository: UsersRepository by lazy {
+        OfflineUsersRepository(UsersDatabase.getDatabase(context).userDao())
     }
 }
