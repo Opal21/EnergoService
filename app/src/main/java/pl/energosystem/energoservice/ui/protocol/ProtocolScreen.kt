b06ce8a8ed55ac35f9d178f9df0e38166037754d
Marcin Opalinski <marcin.opalinski@grupa-abs.pl>
2023-10-10 17:52:51 +0200

    Implement first version of ProtocolScreen

--- app/src/main/java/pl/energosystem/energoservice/ui/protocol/ProtocolScreen.kt
+++ app/src/main/java/pl/energosystem/energoservice/ui/protocol/ProtocolScreen.kt
@@ -1,7 +1,188 @@
 package pl.energosystem.energoservice.ui.protocol
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardActionScope
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import pl.energosystem.energoservice.data.protocol.Protocol
+import pl.energosystem.energoservice.ui.AppViewModelProvider
 
 @Composable
-fun ProtocolScreen(id: String?, onSubmit: () -> Unit) {
+fun ProtocolScreen(
+    id: String?,
+    modifier: Modifier,
+    viewModel: ProtocolViewModel = viewModel(factory = AppViewModelProvider.Factory),
+    onSubmit: () -> Unit
+) {
+    val uiState = viewModel.uiState.collectAsState()
+    val focusManager = LocalFocusManager.current
+
+    ProtocolScreenContent(
+        errorMessage = uiState.value.errorMessage,
+        locatorsNameFieldValue = uiState.value.locatorNameTextField,
+        onLocatorsNameChange = viewModel::onLocatorsNameChange,
+        commentsFieldValue = uiState.value.commentsTextField,
+        onCommentsChange = viewModel::onCommentsChange,
+        serviceType = uiState.value.serviceType,
+        onServiceTypeChange = viewModel::onServiceTypeChange,
+        onNext = { focusManager.moveFocus(FocusDirection.Down) },
+        onDone = viewModel::saveProtocol,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ProtocolScreenContent(
+    errorMessage: String,
+    locatorsNameFieldValue: String,
+    onLocatorsNameChange: (String) -> Unit,
+    commentsFieldValue: String,
+    onCommentsChange: (String) -> Unit,
+    serviceType: ServiceType?,
+    onServiceTypeChange: (ServiceType) -> Unit,
+    onNext: KeyboardActionScope.() -> Unit,
+    onDone: (Protocol) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = errorMessage,
+            style = MaterialTheme.typography.headlineSmall
+        )
+
+        LocatorsNameTextField(
+            locatorsNameFieldValue = locatorsNameFieldValue,
+            onLocatorsNameChange = onLocatorsNameChange,
+            onNext = onNext
+        )
+
+        CommentsTextField(
+            commentsFieldValue = commentsFieldValue,
+            onCommentsChange = onCommentsChange,
+            onNext = onNext
+        )
+
+        ServiceTypeField(
+            serviceType = serviceType,
+            onServiceTypeChange = onServiceTypeChange,
+            onDone = onDone
+        )
+    }
+}
+
+@Composable
+fun LocatorsNameTextField(
+    locatorsNameFieldValue: String,
+    onLocatorsNameChange: (String) -> Unit,
+    onNext: KeyboardActionScope.() -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = locatorsNameFieldValue,
+        onValueChange = onLocatorsNameChange,
+        label = { Text(text = "Locators name") },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(onNext = onNext),
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun CommentsTextField(
+    commentsFieldValue: String,
+    onCommentsChange: (String) -> Unit,
+    onNext: KeyboardActionScope.() -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = commentsFieldValue,
+        onValueChange = onCommentsChange,
+        label = { Text(text = "Comments") },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(onNext = onNext),
+        modifier = modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 40.dp)
+    )
+}
+
+@Composable
+fun ServiceTypeField(
+    serviceType: ServiceType?,
+    onServiceTypeChange: (ServiceType) -> Unit,
+    onDone: (Protocol) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val types = listOf(ServiceType.INSTALLATION, ServiceType.FIX, ServiceType.REPLACEMENT)
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(types[0] ) }
+
+    Column {
+        types.forEach {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = (it == selectedOption),
+                        onClick = { onOptionSelected(it) }
+                    )
+            ) {
+                RadioButton(selected = (it == selectedOption), onClick = { onOptionSelected(it) })
+                Text(
+                    text = it.toString(),
+                    modifier = Modifier.padding(vertical = 16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+fun ProtocolScreenContentPreview() {
+    ProtocolScreenContent(
+        errorMessage = "This is an error!",
+        locatorsNameFieldValue = "",
+        onLocatorsNameChange = {  },
+        commentsFieldValue = "",
+        onCommentsChange = {  },
+        serviceType = ServiceType.INSTALLATION,
+        onServiceTypeChange = {  },
+        onNext = {  },
+        onDone = {  },
+    )
 }
--- app/src/main/java/pl/energosystem/energoservice/ui/protocol/ProtocolViewModel.kt
+++ app/src/main/java/pl/energosystem/energoservice/ui/protocol/ProtocolViewModel.kt
@@ -1,10 +1,58 @@
 package pl.energosystem.energoservice.ui.protocol
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import pl.energosystem.energoservice.data.protocol.Protocol
 import pl.energosystem.energoservice.data.protocol.ProtocolsRepository
 
 class ProtocolViewModel(
-    protocolsRepository: ProtocolsRepository
+    private val protocolsRepository: ProtocolsRepository
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(ProtocolUiState())
+    val uiState: StateFlow<ProtocolUiState> = _uiState
+
+    fun saveProtocol(protocol: Protocol) {
+        if (allFieldsAreFull()) {
+            viewModelScope.launch {
+                protocolsRepository.insertProtocol(protocol)
+            }
+        } else {
+            _uiState.value = _uiState.value.copy(errorMessage = "Fill all fields before saving!")
+        }
+    }
+
+    private fun allFieldsAreFull() =
+        uiState.value.locatorNameTextField.isNotBlank() && uiState.value.serviceType != null
+
+    fun onLocatorsNameChange(newName: String) {
+        if (newName != uiState.value.locatorNameTextField)
+            _uiState.value = _uiState.value.copy(locatorNameTextField = newName)
+    }
+
+    fun onCommentsChange(newComments: String) {
+        if (newComments != uiState.value.commentsTextField)
+            _uiState.value = _uiState.value.copy(commentsTextField = newComments)
+    }
+
+    fun onServiceTypeChange(newServiceType: ServiceType) {
+        if (newServiceType != uiState.value.serviceType)
+            _uiState.value = _uiState.value.copy(serviceType = newServiceType)
+    }
 }
+
+enum class ServiceType {
+    INSTALLATION,
+    REPLACEMENT,
+    FIX
+}
+
+data class ProtocolUiState(
+    val locatorNameTextField: String = "",
+    val commentsTextField: String = "",
+    val serviceType: ServiceType? = null,
+    val errorMessage: String = "",
+)
