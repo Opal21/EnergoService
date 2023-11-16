79c8f4bfca27fc991264c14995102fd98f917c70
Marcin Opalinski <marcinopalinskito@gmail.com>
2023-11-16 17:56:20 +0100

    Update versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "pl.energosystem.energoservice"
         minSdk = 29
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
